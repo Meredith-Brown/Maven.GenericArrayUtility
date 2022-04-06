d21212f445d59cf0e03c585f30c030b04463232b
meredith <meredithbrown302@gmail.com>
2022-04-06 11:54:59 -0400

    GetMostCommon method

--- src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
@@ -2,6 +2,7 @@ package com.zipcodewilmington.arrayutility;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 
 /**
  * Created by leon on 3/6/18.
@@ -31,7 +32,43 @@ public class ArrayUtility<T> {
     }
 
     public T getMostCommonFromMerge(T[] arrayToMerge) {
-        return null;
+        ArrayList<T> withDuplicates = new ArrayList<>();
+        for (int i = 0; i < array.length; i++) {
+            withDuplicates.add(array[i]);
+        }
+        for (int j = 0; j < arrayToMerge.length; j++) {
+            withDuplicates.add(arrayToMerge[j]);
+        }
+
+        ArrayList<T> arrayList = new ArrayList<>(); // no duplicates
+        for (T t : withDuplicates) {
+            if (arrayList.contains(t)) {
+
+            } else {
+                arrayList.add(t);
+            }
+        }
+
+        ArrayList<Integer> frequencyList = new ArrayList<>();
+        for (T t : arrayList) {
+            Integer count = 0;
+            for (T t2 : withDuplicates) {
+                if (t2 == t) {
+                    count++;
+                }
+            }
+            frequencyList.add(count);
+        }
+
+        T output = arrayList.get(0);
+        Integer outputFrequency = frequencyList.get(0);
+        for (int k = 1; k < arrayList.size(); k++) {
+            if (frequencyList.get(k) > outputFrequency) {
+                output = arrayList.get(k);
+                outputFrequency = frequencyList.get(k);
+            }
+        }
+        return output;
     }
 
     public Integer getNumberOfOccurrences(T valueToEvaluate) {
@@ -45,6 +82,16 @@ public class ArrayUtility<T> {
     }
 
     public T[] removeValue(T valueToRemove) {
-        return null;
+        ArrayList<T> arrayList = new ArrayList<>();
+        for (T t : array) {
+            if (t != valueToRemove) {
+                arrayList.add(t);
+            }
+        }
+        T[] output = arrayList.toArray(new T[0]);
+        for (int i = 0; i < arrayList.size(); i++) {
+            output[i] = arrayList.get(i);
+        }
+        return output;
     }
 }
