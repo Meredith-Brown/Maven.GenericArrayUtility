15ecfa53aaf8ebc4e35f060f26173a12b5a67791
meredith <meredithbrown302@gmail.com>
2022-04-05 22:38:31 -0400

    CountDuplicateMerge method

--- src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
@@ -1,7 +1,44 @@
 package com.zipcodewilmington.arrayutility;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /**
  * Created by leon on 3/6/18.
  */
-public class ArrayUtility {
+public class ArrayUtility<T> {
+    T[] array;
+
+    public ArrayUtility(T[] inputArray){
+        array = inputArray;
+    }
+
+    public Integer countDuplicatesInMerge(T[] arrayToMerge, T valueToEvaluate) {
+        ArrayList<T> arrayList = new ArrayList<>();
+        for (int i = 0; i < array.length; i++) {
+            arrayList.add(array[i]);
+        }
+        for (int j = 0; j < arrayToMerge.length; j++) {
+            arrayList.add(arrayToMerge[j]);
+        }
+        Integer count = 0;
+        for (T t : arrayList) {
+            if (t == valueToEvaluate) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public T getMostCommonFromMerge(T[] arrayToMerge) {
+        return null;
+    }
+
+    public Integer getNumberOfOccurrences(T valueToEvaluate) {
+        return null;
+    }
+
+    public T[] removeValue(T valueToRemove) {
+        return null;
+    }
 }
